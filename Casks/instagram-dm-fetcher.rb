fec55da1ac6865dc747a4107179c6501eb5e62fe
Mehdi Oulad Khlie <mehdi.ouladkhlie@icloud.com>
2025-06-09 03:21:59 +0200

    Add Instagram DM Fetcher cask

---
+++ Casks/instagram-dm-fetcher.rb
@@ -0,0 +1,11 @@
+cask "instagram-dm-fetcher" do
+  version "1.0.1"
+  sha256 "PLAK_HIER_DE_SHA256_HASH"
+
+  url "https://github.com/yyyutakaaa/InstaDM-Saver/releases/download/v#{version}/Instagram-DM-Fetcher-v#{version}.dmg"
+  name "Instagram DM Fetcher"
+  desc "Download and backup Instagram direct messages"
+  homepage "https://github.com/yyyutakaaa/InstaDM-Saver"
+
+  app "Instagram DM Fetcher.app"
+end
